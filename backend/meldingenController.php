0a76b8235269df65d3c873c704c98756ee985fb9
ToonBrocks <d284469@edu.rocwb.nl>
2021-03-29 15:44:44 +0200

    fix

--- backend/meldingenController.php
+++ backend/meldingenController.php
@@ -2,9 +2,26 @@
 
 //Variabelen vullen
 $attractie = $_POST['attractie'];
+<<<<<<< Updated upstream
 $group = $_POST['group'];
 $capaciteit = $_POST['capaciteit']; 
+=======
+if (empty($attractie)) {
+    $errors[] = "Vul de attractie-naam in.";
+}
+$group = $_POST['group'];
+if (empty($group)) {
+    $errors[] = "Vul de groep in.";
+}
+$capaciteit = $_POST['capaciteit'];
+if (!is_numeric($capaciteit)) {
+    $errors[] = "Vulvoorcapaciteiteengeldiggetalin.";
+}
+>>>>>>> Stashed changes
 $melder = $_POST['melder'];
+if (empty($attractie)) {
+    $errors[] = "Vul de melder in.";
+}
 
 echo $attractie . " / " . $capaciteit . " / " . $melder;
 
@@ -25,4 +42,9 @@ $statement->execute([
     ":melder" => $melder
 ]);
 
+<<<<<<< Updated upstream
 header("../meldignen/index.php?msg=Melding Opgeslagen");
+=======
+header("../meldingen/index.php");
+?>
+>>>>>>> Stashed changes
--- meldingen/create.php
+++ meldingen/create.php
@@ -22,10 +22,10 @@
             <div class="form-group">
                 <label for="type">Type</label>
                 <select name="group" id="group">
-                    <option value=""> - Kies je klas - </option>
-                    <option value="A">RIO4-AMO1A</option>
-                    <option value="B">RIO4-AMO1B</option>
-                    <option value="C">RIO4-AMO1C</option>
+                    <option value=""> - Kies een type - </option>
+                    <option value="A">Type A</option>
+                    <option value="B">Type B</option>
+                    <option value="C">Type C</option>
                 </select>
             </div>
             <div class="form-group">
@@ -33,8 +33,17 @@
                 <input type="number" min="0" name="capaciteit" id="capaciteit" class="form-input">
             </div>
             <div class="form-group">
+                <label for="prio">Prio:</label>
+                <input type="checkbox" name="prio" id="prio">
+                <label for="prio">Melding met prioriteit</label>
+            </div>
+            <div class="form-group">
                 <label for="melder">Naam melder:</label>
                 <input type="text" name="melder" id="melder" class="form-input">
+            </div>
+            <div class="form-group">
+                <label for="info">Overige info</label>
+                <textarea name="info" id="info" cols="73" rows="4"></textarea>
             </div>
             
             <input type="submit" value="Verstuur melding">
